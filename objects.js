05e9cb7495562daaa1a85262a95db9aeb6469948
craig sperlazza <craig.sperlazza@gmail.com>
2018-01-04 20:59:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,5 @@ var playlist = {name: "title"};
 
 function updatePlaylist (playlistObject, name, title) {
   var playListObject = {name = "title"};
-  playlist = playListObject
-  return playlist
+  return playListObject
 }
